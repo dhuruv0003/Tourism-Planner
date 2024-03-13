6ad186fc5f6a31dcdde0ff3505cee6140a7b8f17
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-14 00:51:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  
+
+  return (
+    <div className=''>
+      <div className="">
+        <h1>Tours Planning by Dhuruv</h1>
+        <Tours></Tours>
+      </div>
+    </div>
+  )
+}
+
+export default App
