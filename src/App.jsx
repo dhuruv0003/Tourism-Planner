17dcc2e8128f9ce9fba66105397c2788c471df38
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-14 00:56:34 +0530

    "new commit"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Tours from './components/Tours'
+import data from './data'
 
 function App() {
 
-  
+  const [data,setData]=useState(data)
 
   return (
     <div className=''>
       <div className="">
         <h1>Tours Planning by Dhuruv</h1>
-        <Tours></Tours>
+        <Tours items={data}></Tours>
       </div>
     </div>
   )
--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,3 +1,3 @@
-export default function tours(){
-    
+export default function Tours(){
+
 }
